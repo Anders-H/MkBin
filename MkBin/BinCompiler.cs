fbb3e621f2e9449b54f6371ad30554e20651c69e
Anders-H <anders@winsoft.se>
2022-01-30 18:01:38 +0100

    Some structure.

--- MkBin/BinCompiler.cs
+++ MkBin/BinCompiler.cs
@@ -2,96 +2,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
-using System.Text.RegularExpressions;
+using MkBin.CompilerService;
 using MkBin.Tokens;
 
 namespace MkBin;
 
 public class BinCompiler
 {
-    private readonly List<string> _parts = new();
-    private readonly AliasList _aliases = new();
+    private readonly List<string> _parts;
 
     public BinCompiler(string source)
     {
-        _parts.Clear();
-        _aliases.Clear();
-
-        var rows = source.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
-
-        foreach (var row in rows)
-        {
-            var r = row.Trim();
-
-            if (r.StartsWith('#'))
-                continue;
-
-            if (r.IndexOf('#') > -1)
-                r = r.Substring(0, r.IndexOf('#')).Trim();
-
-            var alias = AliasCompiler.Compile(r);
-            if (alias != null)
-            {
-                _aliases.Add(alias);
-                continue;
-            }
-
-            var parts = Regex.Matches(r, @"[\""].+?[\""]|[^\s]+");
-
-            foreach (Match part in parts)
-                _parts.Add(part.Value);
-        }
-
-        var newList = new List<string>();
-
-        foreach (var t in _parts)
-        {
-            var toEvaluate = new List<string>();
-
-            if (_aliases.Has(t))
-            {
-                var v = _aliases.Get(t)!.Value;
-                var parts = Regex.Matches(v, @"[\""].+?[\""]|[^\s]+");
-
-                foreach (Match part in parts)
-                    toEvaluate.Add(part.Value);
-            }
-            else
-            {
-                toEvaluate.Add(t);
-            }
-
-            foreach (var iterator in toEvaluate)
-            {
-                var p = iterator;
-                var times = 1;
-
-                var match = Regex.Match(p, @"\*[0-9]+$");
-                if (match.Success)
-                {
-                    var lastIndex = p.LastIndexOf("*", StringComparison.Ordinal);
-                    var timesString = p.Substring(lastIndex + 1);
-                    var valueString = p.Substring(0, lastIndex);
-
-                    if (!int.TryParse(timesString, out times))
-                    {
-                        throw new SystemException($"Failed to parse: {t}");
-                    }
-
-                    p = valueString;
-                }
-
-                var isString = p.StartsWith("\"") && p.EndsWith("\"");
-
-                for (var j = 0; j < times; j++)
-                    newList.Add(
-                        (isString ? "\"" : "") + Regex.Replace(p, @"[^\w:/ ]", string.Empty) + (isString ? "\"" : "")
-                    );
-            }
-        }
-
-        _parts = newList;
+        _parts = new StringSplitter(source)
+            .GetParts();
     }
 
     public byte[] Compile()
---
+++ MkBin/CompilerService/StringSplitter.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using MkBin.CompilerParts;
+
+namespace MkBin.CompilerService
+{
+    public class StringSplitter
+    {
+        private readonly string _source;
+
+        public StringSplitter(string source)
+        {
+            _source = source;
+        }
+
+        public List<string> GetParts()
+        {
+            var rows = _source.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            var aliases = new AliasList();
+            var result = new List<string>();
+
+            foreach (var row in rows)
+            {
+                var r = row.Trim();
+
+                if (r.StartsWith('#'))
+                    continue;
+
+                if (r.IndexOf('#') > -1)
+                    r = r.Substring(0, r.IndexOf('#')).Trim();
+
+                var alias = AliasCompiler.Compile(r);
+                if (alias != null)
+                {
+                    aliases.Add(alias);
+                    continue;
+                }
+
+                var parts = Regex.Matches(r, @"[\""].+?[\""]|[^\s]+");
+
+                foreach (Match part in parts)
+                    result.Add(part.Value);
+            }
+
+            var newList = new List<string>();
+
+            foreach (var t in result)
+            {
+                var toEvaluate = new List<string>();
+
+                if (aliases.Has(t))
+                {
+                    var v = aliases.Get(t)!.Value;
+                    var parts = Regex.Matches(v, @"[\""].+?[\""]|[^\s]+");
+
+                    foreach (Match part in parts)
+                        toEvaluate.Add(part.Value);
+                }
+                else
+                {
+                    toEvaluate.Add(t);
+                }
+
+                foreach (var iterator in toEvaluate)
+                {
+                    var p = iterator;
+                    var times = 1;
+
+                    var match = Regex.Match(p, @"\*[0-9]+$");
+                    if (match.Success)
+                    {
+                        var lastIndex = p.LastIndexOf("*", StringComparison.Ordinal);
+                        var timesString = p.Substring(lastIndex + 1);
+                        var valueString = p.Substring(0, lastIndex);
+
+                        if (!int.TryParse(timesString, out times))
+                            throw new SystemException($"Failed to parse: {t}");
+
+                        p = valueString;
+                    }
+
+                    var isString = p.StartsWith("\"") && p.EndsWith("\"");
+
+                    for (var j = 0; j < times; j++)
+                        newList.Add(
+                            (isString ? "\"" : "") + Regex.Replace(p, @"[^\w:/ ]", string.Empty) + (isString ? "\"" : "")
+                        );
+                }
+            }
+
+            return newList;
+        }
+    }
+}
