6c5ff962a04c805a84b3fa1187f6459b0825dd7e
Anders Hesselbom <anders@winsoft.se>
2022-02-02 21:25:24 +0100

    Text open/save dialog removed.

---
+++ MkBin/Gui/FileDialogs.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Forms;
+
+namespace MkBin.Gui
+{
+    public static class FileDialogs
+    {
+        public static SaveFileDialog GetSaveTextFileDialog(string filename) =>
+            new SaveFileDialog
+            {
+                Title = @"Save text description of binary file",
+                Filter = @"*.txt|*.txt|*.*|*.*",
+                FileName = filename
+            };
+
+        public static OpenFileDialog GetOpenTextFileDialog(string filename) =>
+            new OpenFileDialog()
+            {
+                Title = @"Load text description of binary file",
+                Filter = @"*.txt|*.txt|*.*|*.*",
+                FileName = filename
+            };
+    }
+}
